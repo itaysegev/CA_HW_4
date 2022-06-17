4c75b35918af98071f557c8346fc9d2e7184932f
Itay Segev <itaysegev11@gmail.com>
2022-06-17 18:17:30 +0300

    fine-grained

--- core_api.cpp
+++ core_api.cpp
@@ -161,7 +161,7 @@ void CORE_FinegrainedMT() {
 	vector<tcontext>::iterator itr;
 	for(itr = curr_sim.thread_regs.begin(); itr != curr_sim.thread_regs.end();  itr++) {
 		THREADS_REGS.push_back(*itr);
-		cout << (*itr).reg[0] << endl;
+		cout << (*itr).reg[2] << endl;
 
 	}
 }
