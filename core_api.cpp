c19f83c21b8c3ffc7bc310d3a9ede7228c35d03d
Itay Segev <itaysegev11@gmail.com>
2022-06-17 13:33:26 +0300

    fine-grained

--- core_api.cpp
+++ core_api.cpp
@@ -11,7 +11,7 @@ void CORE_BlockedMT() {
 }
 
 void CORE_FinegrainedMT() {
-	uint32_t line = 0;
+	uint32_t line = 1;
 	Instruction curr_inst;
 	int threads = SIM_GetThreadsNum();
 	SIM_MemInstRead(line, &curr_inst, 1);
