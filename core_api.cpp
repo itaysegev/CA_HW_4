05bcbc0dbcc07f0d0c58d718908d018f2f53a0f9
Itay Segev <itaysegev11@gmail.com>
2022-06-17 12:33:04 +0300

    fine-grained

---
+++ core_api.cpp
@@ -0,0 +1,33 @@
+/* 046267 Computer Architecture - Winter 20/21 - HW #4 */
+
+#include "core_api.h"
+#include "sim_api.h"
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+void CORE_BlockedMT() {
+}
+
+void CORE_FinegrainedMT() {
+	int threads = SIM_GetThreadsNum();
+	cout << threads << endl;
+	cout << "end" << endl;
+
+
+}
+
+double CORE_BlockedMT_CPI(){
+	return 0;
+}
+
+double CORE_FinegrainedMT_CPI(){
+	return 0;
+}
+
+void CORE_BlockedMT_CTX(tcontext* context, int threadid) {
+}
+
+void CORE_FinegrainedMT_CTX(tcontext* context, int threadid) {
+}
