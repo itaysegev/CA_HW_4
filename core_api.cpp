9325ffc8798147de2c45497edceabd0032bef484
Itay Segev <itaysegev11@gmail.com>
2022-06-18 09:30:31 +0300

    fine-grained

--- core_api.cpp
+++ core_api.cpp
@@ -8,48 +8,52 @@
 #include <vector>
 #include <tuple>
 #define IDLE -1
-
+#define INIT_VAL 0
 using namespace std;
-vector<tcontext> THREADS_REGS;
-double INST = 0;
-int CYCLE = 0;
 
+//global var for calculation function 
+vector<tcontext> THREADS_REGS;
+double INST = INIT_VAL;
+int CYCLE = INIT_VAL;
+
+//main data struct all other data structures inherits from it
 class simulation {
 	protected:
 		int cycle;
 		int threads_num;
-		set<int> threads_pool;
-		set <int> finished_t;
-		set <tuple<int,int>> wait_t; // (thread_id, return_cycle)
-		vector<int> line_num;
+		set<int> threads_pool; // all ready to run threads
+		set <int> finished_t; // all finished threads
+		set <tuple<int,int>> wait_t; // (thread_id, return_cycle) for waiting state threads
+		vector<int> line_num; // line number in code for each thread
 	public:
-		vector<tcontext> thread_regs;
-		simulation(int threads_num) : threads_num(threads_num), cycle(0) {
+		vector<tcontext> thread_regs; // regs context for each thread later copy to global 
+		simulation(int threads_num) : threads_num(threads_num), cycle(INIT_VAL) {
 			tcontext init_regs;
+			// init all with zeros and all threads are ready to run
 			for(int i = 0; i < REGS_COUNT; i++) {
-				init_regs.reg[i] = 0;
+				init_regs.reg[i] = INIT_VAL;
 			}
 			for (int i = 0; i< threads_num; i++){
 				threads_pool.insert(i);
-				line_num.push_back(0);
+				line_num.push_back(INIT_VAL);
 				thread_regs.push_back(init_regs);
 			}
 		}
 		int getCycle() {return cycle;}
 		int getThreadsNum() {return threads_num;}
 		void wait(int tid, int wait_cycle) {
-			threads_pool.erase(tid);
-			int return_cycle = cycle + wait_cycle + 1;
-			wait_t.insert(make_tuple(tid, return_cycle));
+			threads_pool.erase(tid); // remove from ready to run state
+			int return_cycle = cycle + wait_cycle + 1; // calculate return cycle number
+			wait_t.insert(make_tuple(tid, return_cycle)); // insert to waiting list
 		}
 		bool idle() {return threads_pool.empty();}
 		void threadEnded(int tid) {
 			finished_t.insert(tid);
 			threads_pool.erase(tid);
 		}
-		bool simEnded() {return finished_t.size() == threads_num;}
+		bool simEnded() {return finished_t.size() == threads_num;} 
 		int getNextLine(int tid) {return line_num[tid];}
-		void aritAct(Instruction inst, int tid) {
+		void aritAct(Instruction inst, int tid) { // all regs op by different opcode as define in sim_api.h
 			if(inst.opcode == CMD_ADD) {
 				thread_regs[tid].reg[inst.dst_index] = thread_regs[tid].reg[inst.src1_index] + thread_regs[tid].reg[inst.src2_index_imm];
 			}
@@ -63,7 +67,7 @@ class simulation {
 				thread_regs[tid].reg[inst.dst_index] = thread_regs[tid].reg[inst.src1_index] - inst.src2_index_imm;
 			}
 		}
-		void memAct(Instruction inst, int tid) {
+		void memAct(Instruction inst, int tid) { // all memory op by opcode as define in sim_api.h
 			int sec_op;
 			if(inst.isSrc2Imm) {
 				sec_op = inst.src2_index_imm;
@@ -81,6 +85,7 @@ class simulation {
 		void endCycle(int tid) {
 			cycle ++;
 			set <tuple<int,int>>::iterator itr;
+			// return to ready to run all threads that have finished waiting time
 			for(itr = wait_t.begin(); itr != wait_t.end(); itr++) {
 				if(cycle == get<1>(*itr)){
 					int return_t = get<0>(*itr);
@@ -93,14 +98,11 @@ class simulation {
 
 		}
 };
-
+// fine-grained architecture data structure
 class fine_grained: public simulation {
-	private:
-		int inst_num;
 	public:
-		fine_grained(int threads_num) : simulation(threads_num), inst_num(0) {}
-		int nextThread(int tid) {
-			
+		fine_grained(int threads_num) : simulation(threads_num) {}
+		int nextThread(int tid) { // schedular policy by fine-grained definition 
 			int next_tid = IDLE;
 			if(idle()){
 				return next_tid;
@@ -130,11 +132,10 @@ void CORE_FinegrainedMT() {
 	if (threads_num == 0){
 		return;
 	}
-	int curr_tid = 0;
+	int curr_tid = INIT_VAL;
 	fine_grained curr_sim(SIM_GetThreadsNum());
 	Instruction curr_inst;
 	while(!curr_sim.simEnded()) {
-		cout << "curr_tid: "<< curr_tid << endl;
 		if(curr_tid != IDLE) {
 			INST++;
 			SIM_MemInstRead(curr_sim.getNextLine(curr_tid), &curr_inst, curr_tid);
@@ -171,8 +172,8 @@ double CORE_BlockedMT_CPI(){
 
 double CORE_FinegrainedMT_CPI(){
 	double cpi = CYCLE / INST;
-	CYCLE = 0;
-	INST = 0; 
+	CYCLE = INIT_VAL;
+	INST = INIT_VAL; 
 	return cpi;
 }
 
